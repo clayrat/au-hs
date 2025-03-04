4d59d1968c29d71f74e5214d0df85770581f228a
Alex Gryzlov <aliaksandr.hryzlou@imdea.org>
2025-03-04 19:02:48 +0100

    better types

--- app/AU.hs
+++ app/AU.hs
@@ -1,12 +1,21 @@
 module AU where
 
-import Data.Maybe (fromMaybe)
+import Data.Maybe (maybe)
 import Data.Char (ord, chr)
 
-data Term = Var String
-          | Sym String
+newtype Id = Id String deriving (Eq, Show)
+newtype Sy = Sy String deriving (Eq, Show)
+
+data Term = Var Id
+          | Sym Sy
           | Cons Term Term
           deriving (Eq, Show)
+
+var :: String -> Term
+var = Var . Id
+
+sym :: String -> Term
+sym = Sym . Sy
 
 isCons :: Term -> Bool
 isCons (Cons _ _) = True
@@ -14,8 +23,11 @@ isCons _ = False
 
 type Subst a b = [(a, b)]
 
-genny :: Char -> Int -> String
-genny c n = c : '_' : [chr (ord '0' + n)]
+genid :: Int -> Id
+genid n = Id ("z_" ++ [chr (ord '0' + n)])
+
+gensy :: Int -> Sy
+gensy n = Sy ("c_" ++ [chr (ord '0' + n)])
 
 lookupSubst :: Eq a => a -> Subst a b -> Maybe b
 lookupSubst _   []           = Nothing
@@ -24,29 +36,29 @@ lookupSubst key ((k,v):rest) = if key == k then Just v else lookupSubst key rest
 invertSubst :: Subst a b -> Subst b a
 invertSubst = map (\(x, y) -> (y, x))
 
-preProcess :: [Term] -> ([Term], Subst Term Term)
+preProcess :: [Term] -> ([Term], Subst Id Sy)
 preProcess terms =
   let (t, s, _) = foldl processOne ([], [], 0) terms in (t, s)
   where
-    processOne :: ([Term], Subst Term Term, Int) -> Term -> ([Term], Subst Term Term, Int)
+    processOne :: ([Term], Subst Id Sy, Int) -> Term -> ([Term], Subst Id Sy, Int)
     processOne (accTerms, subst, n) term =
       let (term', subst', n') = preProcess' term subst n
       in (accTerms ++ [term'], subst', n')
 
-    preProcess' :: Term -> Subst Term Term -> Int -> (Term, Subst Term Term, Int)
+    preProcess' :: Term -> Subst Id Sy -> Int -> (Term, Subst Id Sy, Int)
     preProcess' (Cons a d) subst n =
       let (a', subst' , n' ) = preProcess' a subst  n
           (d', subst'', n'') = preProcess' d subst' n'
       in (Cons a' d', subst'', n'')
-    preProcess' v@(Var _) subst n =
+    preProcess' (Var v) subst n =
       case lookupSubst v subst of
-        Just c -> (c, subst, n)
-        Nothing -> let c = Sym (genny 'c' n)
+        Just c -> (Sym c, subst, n)
+        Nothing -> let c = gensy n
                        subst' = (v, c) : subst
-                   in (c, subst', n + 1)
+                   in (Sym c, subst', n + 1)
     preProcess' t subst n = (t, subst, n)
 
-auTheta :: [Term] -> Subst [Term] Term -> Int -> (Term, Subst [Term] Term, Int)
+auTheta :: [Term] -> Subst [Term] Id -> Int -> (Term, Subst [Term] Id, Int)
 auTheta terms theta n =
   if null terms
     then error "auTheta: |terms| must be > 0"
@@ -58,15 +70,15 @@ auTheta terms theta n =
             (s', theta'', n'') = auTheta [t2 | Cons _ t2 <- ts] theta' n'
         in (Cons s s', theta'', n'')
       ts | Just x <- lookupSubst ts theta ->  -- Rule 9
-        (x, theta, n)
+        (Var x, theta, n)
       ts ->  -- Rule 10
-        let z = Var (genny 'z' n)
+        let z = genid n
             theta' = (ts, z) : theta
-        in (z, theta', n + 1)
+        in (Var z, theta', n + 1)
 
-postProcess :: Term -> Subst Term Term -> Term
+postProcess :: Term -> Subst Sy Id -> Term
 postProcess   (Cons a d) subst = Cons (postProcess a subst) (postProcess d subst)
-postProcess c@(Sym _)    subst = fromMaybe c (lookupSubst c subst)
+postProcess c@(Sym s)    subst = maybe c Var (lookupSubst s subst)
 postProcess t            _     = t
 
 -- Anti-unification
@@ -75,6 +87,6 @@ au terms =
   if null terms
     then error "au: |terms| must be > 0"
     else let (terms', subst) = preProcess terms
-             (s, _, _) = auTheta terms' [] 0  -- Rule 6
              invSubst = invertSubst subst
+             (s, _, _) = auTheta terms' [] 0  -- Rule 6
          in postProcess s invSubst
--- app/Main.hs
+++ app/Main.hs
@@ -9,155 +9,155 @@ import AU
 tests :: Test
 tests = TestList [
     TestLabel "pre-process-1" $ TestCase $
-      let (t, subst) = preProcess [Cons (Sym "5") (Sym "5")]
+      let (t, subst) = preProcess [Cons (sym "5") (sym "5")]
       in assertEqual "Simple pre-process"
-         ([Cons (Sym "5") (Sym "5")], [])
+         ([Cons (sym "5") (sym "5")], [])
          (t, subst)
 
   , TestLabel "pre-process-2" $ TestCase $
-      let (t, _) = preProcess [Cons (Var "x") (Sym "5")]
+      let (t, _) = preProcess [Cons (var "x") (sym "5")]
       in assertEqual "Pre-process with variable"
-         [Cons (Sym "c_0") (Sym "5")]
+         [Cons (sym "c_0") (sym "5")]
          t
 
   , TestLabel "pre-process-3" $ TestCase $
-      let x = Var "x"
-          y = Var "y"
-          input = [Cons x (Cons (Sym "5") (Cons y (Cons x y)))]
+      let x = var "x"
+          y = var "y"
+          input = [Cons x (Cons (sym "5") (Cons y (Cons x y)))]
           (t, _) = preProcess input
       in assertEqual "Complex pre-process"
-         [Cons (Sym "c_0") (Cons (Sym "5") (Cons (Sym "c_1") (Cons (Sym "c_0") (Sym "c_1"))))]
+         [Cons (sym "c_0") (Cons (sym "5") (Cons (sym "c_1") (Cons (sym "c_0") (sym "c_1"))))]
          t
 
   , TestLabel "post-process-1" $ TestCase $
-      let x = Var "x"
-          y = Var "y"
-          input = [Cons x (Cons (Sym "5") (Cons y (Cons x y)))]
+      let x = var "x"
+          y = var "y"
+          input = [Cons x (Cons (sym "5") (Cons y (Cons x y)))]
           (_, subst) = preProcess input
           invSubst = invertSubst subst
-          processed = Cons (Sym "c_0") (Cons (Sym "5") (Cons (Sym "c_1") (Cons (Sym "c_0") (Sym "c_1"))))
+          processed = Cons (sym "c_0") (Cons (sym "5") (Cons (sym "c_1") (Cons (sym "c_0") (sym "c_1"))))
       in assertEqual "Post-process"
          input
          [postProcess processed invSubst]
 
   , TestLabel "au-1" $ TestCase $
-      let input = [Sym "5" , Sym "5"]
+      let input = [sym "5" , sym "5"]
       in assertEqual "Simple anti-unification"
-         (Sym "5")
+         (sym "5")
          (au input)
 
   , TestLabel "au-2" $ TestCase $
-      let input = [Sym "5" , Sym "6"]
+      let input = [sym "5" , sym "6"]
       in assertEqual "Different constants"
-         (Var "z_0")
+         (var "z_0")
          (au input)
 
   , TestLabel "au-3" $ TestCase $
-      let input = [Sym "5", Sym "6", Sym "7"]
+      let input = [sym "5", sym "6", sym "7"]
       in assertEqual "Three different constants"
-         (Var "z_0")
+         (var "z_0")
          (au input)
 
   , TestLabel "au-4" $ TestCase $
-      let x = Var "x"
-          y = Var "y"
-          t1 = Cons (Sym "c") (Cons x (Cons (Sym "c") x))
-          t2 = Cons (Sym "d") (Cons x (Cons (Sym "d") y))
-          z0 = Var "z_0"
-          z1 = Var "z_1"
+      let x = var "x"
+          y = var "y"
+          t1 = Cons (sym "c") (Cons x (Cons (sym "c") x))
+          t2 = Cons (sym "d") (Cons x (Cons (sym "d") y))
+          z0 = var "z_0"
+          z1 = var "z_1"
           expected = Cons z0 (Cons x (Cons z0 z1))
       in assertEqual "Complex anti-unification"
          expected
          (au [t1, t2])
 
   , TestLabel "au-4b" $ TestCase $
-      let x = Var "x"
-          y = Var "y"
-          t1 = Cons (Sym "c") (Cons y (Cons (Sym "c") x))
-          t2 = Cons (Sym "d") (Cons x (Cons (Sym "d") y))
-          z0 = Var "z_0"
-          z1 = Var "z_1"
-          z2 = Var "z_2"
+      let x = var "x"
+          y = var "y"
+          t1 = Cons (sym "c") (Cons y (Cons (sym "c") x))
+          t2 = Cons (sym "d") (Cons x (Cons (sym "d") y))
+          z0 = var "z_0"
+          z1 = var "z_1"
+          z2 = var "z_2"
           expected = Cons z0 (Cons z1 (Cons z0 z2))
       in assertEqual "Complex anti-unification with different variables"
          expected
          (au [t1, t2])
 
   , TestLabel "au-5" $ TestCase $
-      let x = Var "x"
-          y = Var "y"
-          t1 = Cons (Sym "c") (Cons x (Cons (Sym "c") x))
-          t2 = Cons (Sym "d") (Cons x (Cons (Sym "d") y))
-          t3 = Cons (Sym "d") (Cons x (Cons (Sym "d") y))
-          z0 = Var "z_0"
-          z1 = Var "z_1"
+      let x = var "x"
+          y = var "y"
+          t1 = Cons (sym "c") (Cons x (Cons (sym "c") x))
+          t2 = Cons (sym "d") (Cons x (Cons (sym "d") y))
+          t3 = Cons (sym "d") (Cons x (Cons (sym "d") y))
+          z0 = var "z_0"
+          z1 = var "z_1"
           expected = Cons z0 (Cons x (Cons z0 z1))
       in assertEqual "Three terms anti-unification"
          expected
          (au [t1, t2, t3])
 
   , TestLabel "au-6" $ TestCase $
-      let t1 = Cons (Sym "lambda") (Cons (Sym "5") (Sym "5"))
-          t2 = Cons (Sym "lambda") (Cons (Sym "t") (Sym "t"))
-          z0 = Var "z_0"
-          expected = Cons (Sym "lambda") (Cons z0 z0)
+      let t1 = Cons (sym "lambda") (Cons (sym "5") (sym "5"))
+          t2 = Cons (sym "lambda") (Cons (sym "t") (sym "t"))
+          z0 = var "z_0"
+          expected = Cons (sym "lambda") (Cons z0 z0)
       in assertEqual "Lambda abstraction"
          expected
          (au [t1, t2])
 
   , TestLabel "au-7" $ TestCase $
-      let t1 = Cons (Sym "lambda") (Cons (Sym "5") (Sym "5"))
-          t2 = Cons (Sym "lambda") (Cons (Sym "t") (Sym "t"))
-          t3 = Cons (Sym "lambda") (Cons (Sym "5") (Sym "6"))
-          z0 = Var "z_0"
-          z1 = Var "z_1"
-          expected = Cons (Sym "lambda") (Cons z0 z1)
+      let t1 = Cons (sym "lambda") (Cons (sym "5") (sym "5"))
+          t2 = Cons (sym "lambda") (Cons (sym "t") (sym "t"))
+          t3 = Cons (sym "lambda") (Cons (sym "5") (sym "6"))
+          z0 = var "z_0"
+          z1 = var "z_1"
+          expected = Cons (sym "lambda") (Cons z0 z1)
       in assertEqual "Lambda abstraction with three terms"
          expected
          (au [t1, t2, t3])
 
   , TestLabel "au-8" $ TestCase $
-      let t1 = Cons (Sym "lambda") (Cons (Sym "5") (Sym "5"))
-          t2 = Cons (Sym "lambda") (Cons (Sym "t") (Sym "t"))
-          x = Var "x"
-          t_lhs = Cons (Sym "lambda") (Cons x x)
+      let t1 = Cons (sym "lambda") (Cons (sym "5") (sym "5"))
+          t2 = Cons (sym "lambda") (Cons (sym "t") (sym "t"))
+          x = var "x"
+          t_lhs = Cons (sym "lambda") (Cons x x)
           t_rhs = au [t1, t2]
-          z0 = Var "z_0"
-          expected = Cons (Sym "lambda") (Cons z0 z0)
+          z0 = var "z_0"
+          expected = Cons (sym "lambda") (Cons z0 z0)
       in assertEqual "Lambda abstraction comparison"
          expected
          (au [t_lhs, t_rhs])
 
   , TestLabel "au-9" $ TestCase $
-      let t1 = Cons (Sym "lambda") (Cons (Sym "5") (Sym "5"))
-          t2 = Cons (Sym "lambda") (Cons (Sym "t") (Sym "t"))
-          t3 = Cons (Sym "lambda") (Cons (Sym "5") (Sym "6"))
-          x = Var "x"
-          t_lhs = Cons (Sym "lambda") (Cons x x)
+      let t1 = Cons (sym "lambda") (Cons (sym "5") (sym "5"))
+          t2 = Cons (sym "lambda") (Cons (sym "t") (sym "t"))
+          t3 = Cons (sym "lambda") (Cons (sym "5") (sym "6"))
+          x = var "x"
+          t_lhs = Cons (sym "lambda") (Cons x x)
           t_rhs = au [t1, t2, t3]
-          z0 = Var "z_0"
-          z1 = Var "z_1"
-          expected = Cons (Sym "lambda") (Cons z0 z1)
+          z0 = var "z_0"
+          z1 = var "z_1"
+          expected = Cons (sym "lambda") (Cons z0 z1)
       in assertEqual "Lambda abstraction comparison with three terms"
          expected
          (au [t_lhs, t_rhs])
 
   , TestLabel "au-10" $ TestCase $
-      let x = Var "x"
-          y = Var "y"
-          t1 = Cons (Sym "lambda") (Cons x x)
-          t2 = Cons (Sym "lambda") (Cons y y)
-          z0 = Var "z_0"
-          expected = Cons (Sym "lambda") (Cons z0 z0)
+      let x = var "x"
+          y = var "y"
+          t1 = Cons (sym "lambda") (Cons x x)
+          t2 = Cons (sym "lambda") (Cons y y)
+          z0 = var "z_0"
+          expected = Cons (sym "lambda") (Cons z0 z0)
       in assertEqual "Lambda abstraction with different variables"
          expected
          (au [t1, t2])
 
   , TestLabel "au-11" $ TestCase $
-      let x = Var "x"
-          t1 = Cons (Sym "lambda") (Cons x x)
-          t2 = Cons (Sym "lambda") (Cons x x)
-          expected = Cons (Sym "lambda") (Cons x x)
+      let x = var "x"
+          t1 = Cons (sym "lambda") (Cons x x)
+          t2 = Cons (sym "lambda") (Cons x x)
+          expected = Cons (sym "lambda") (Cons x x)
       in assertEqual "Lambda abstraction with same variable"
          expected
          (au [t1, t2])
