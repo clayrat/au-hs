52ee00c0e898862a1df1564c828071e9c2f6dee4
Alex Gryzlov <aliaksandr.hryzlou@imdea.org>
2025-03-04 21:08:19 +0100

    refactor to use state monad

--- app/AU.hs
+++ app/AU.hs
@@ -1,6 +1,6 @@
 module AU where
 
-import Data.Maybe (maybe)
+import Control.Monad.State
 import Data.Char (ord, chr)
 
 newtype Id = Id String deriving (Eq, Show)
@@ -19,15 +19,16 @@ sym = Sym . Sy
 
 isCons :: Term -> Bool
 isCons (Cons _ _) = True
-isCons _ = False
+isCons _          = False
 
 type Subst a b = [(a, b)]
 
-genid :: Int -> Id
-genid n = Id ("z_" ++ [chr (ord '0' + n)])
-
-gensy :: Int -> Sy
-gensy n = Sy ("c_" ++ [chr (ord '0' + n)])
+gen :: Char -> (String -> b) -> a -> State (Int, Subst a b) b
+gen c f a = do
+  (n, s) <- get
+  let b = f (c : '_' : [chr (ord '0' + n)])
+  put (n + 1, (a, b) : s)
+  return b
 
 lookupSubst :: Eq a => a -> Subst a b -> Maybe b
 lookupSubst _   []           = Nothing
@@ -38,43 +39,41 @@ invertSubst = map (\(x, y) -> (y, x))
 
 preProcess :: [Term] -> ([Term], Subst Id Sy)
 preProcess terms =
-  let (t, s, _) = foldl processOne ([], [], 0) terms in (t, s)
+  let (t, (_ , s)) = runState (mapM preProcess' terms) (0, [])
+  in (t , s)
   where
-    processOne :: ([Term], Subst Id Sy, Int) -> Term -> ([Term], Subst Id Sy, Int)
-    processOne (accTerms, subst, n) term =
-      let (term', subst', n') = preProcess' term subst n
-      in (accTerms ++ [term'], subst', n')
+    preProcess' :: Term -> State (Int, Subst Id Sy) Term
+    preProcess' (Cons a d) =
+      do a' <- preProcess' a
+         d' <- preProcess' d
+         pure (Cons a' d')
+    preProcess' (Var v) =
+      do sub <- gets snd
+         case lookupSubst v sub of
+           Just c -> pure (Sym c)
+           Nothing ->
+             do c <- gen 'c' Sy v
+                pure (Sym c)
+    preProcess' t = pure t
 
-    preProcess' :: Term -> Subst Id Sy -> Int -> (Term, Subst Id Sy, Int)
-    preProcess' (Cons a d) subst n =
-      let (a', subst' , n' ) = preProcess' a subst  n
-          (d', subst'', n'') = preProcess' d subst' n'
-      in (Cons a' d', subst'', n'')
-    preProcess' (Var v) subst n =
-      case lookupSubst v subst of
-        Just c -> (Sym c, subst, n)
-        Nothing -> let c = gensy n
-                       subst' = (v, c) : subst
-                   in (Sym c, subst', n + 1)
-    preProcess' t subst n = (t, subst, n)
-
-auTheta :: [Term] -> Subst [Term] Id -> Int -> (Term, Subst [Term] Id, Int)
-auTheta terms theta n =
+auTheta :: [Term] -> State (Int, Subst [Term] Id) Term
+auTheta terms =
   if null terms
     then error "auTheta: |terms| must be > 0"
-    else case terms of
-      (t:ts) | all (== t) ts ->  -- Rule 7
-        (t, theta, n)
-      ts | all isCons ts ->  -- Rule 8
-        let (s , theta' , n' ) = auTheta [t1 | Cons t1 _ <- ts] theta  n
-            (s', theta'', n'') = auTheta [t2 | Cons _ t2 <- ts] theta' n'
-        in (Cons s s', theta'', n'')
-      ts | Just x <- lookupSubst ts theta ->  -- Rule 9
-        (Var x, theta, n)
-      ts ->  -- Rule 10
-        let z = genid n
-            theta' = (ts, z) : theta
-        in (Var z, theta', n + 1)
+    else
+      do theta <- gets snd
+         case terms of
+           (t:ts) | all (== t) ts ->  -- Rule 7
+             pure t
+           ts | all isCons ts ->  -- Rule 8
+             do s  <- auTheta [t1 | Cons t1 _ <- ts]
+                s' <- auTheta [t2 | Cons _ t2 <- ts]
+                pure (Cons s s')
+           ts | Just x <- lookupSubst ts theta ->  -- Rule 9
+             pure (Var x)
+           _ ->  -- Rule 10
+             do z <- gen 'z' Id terms
+                pure (Var z)
 
 postProcess :: Term -> Subst Sy Id -> Term
 postProcess   (Cons a d) subst = Cons (postProcess a subst) (postProcess d subst)
@@ -88,5 +87,5 @@ au terms =
     then error "au: |terms| must be > 0"
     else let (terms', subst) = preProcess terms
              invSubst = invertSubst subst
-             (s, _, _) = auTheta terms' [] 0  -- Rule 6
+             s = evalState (auTheta terms') (0, [])  -- Rule 6
          in postProcess s invSubst
